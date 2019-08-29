5aad76e8bc1e55d92b707b3b0def4b8e05440377
Mateusz Zbylut <mateusz.zbylut@gate-software.com>
2019-08-29 15:34:36 +0200

    Task 3

--- lib/CacheWarm.php
+++ lib/CacheWarm.php
@@ -52,6 +52,17 @@ class Old_Legacy_CacheWarmer_Warmer
         $this->hostname = $hostname;
     }
 
+    public function updateLastVisited($hostname, $url)
+    {
+        $db = new Snowdog\DevTest\Core\Database();
+        $website_id = $db->query('SELECT `website_id` FROM `websites` WHERE `hostname`= "' . $hostname . '"');
+        $page_id = $db->query('SELECT `page_id` FROM `pages` WHERE `url` = "' . $url . '" AND `website_id`= "' .
+            $website_id->fetch()['website_id'] . '"');
+        $date = date('Y-m-d H:i:s');
+        $update = $db->prepare('UPDATE `pages` SET `last_visited` = ? WHERE `page_id` = ?');
+        $update->execute(array($date, $page_id->fetch()['page_id']));
+    }
+
     /**
      * @param Old_Legacy_CacheWarmer_Resolver_Interface $resolver
      */
@@ -64,7 +75,7 @@ class Old_Legacy_CacheWarmer_Warmer
         $ip = $this->resolver->getIp($this->hostname);
         sleep(1); // this emulates visit to http://$hostname/$url via $ip
         $this->actor->act($this->hostname, $ip, $url);
+        $this->updateLastVisited($this->hostname, $url);
     }
-    
 }
 
